1034e70d48c2d48fc680a086427a1b5d49069db5
ymim <43496855+ymimsr@users.noreply.github.com>
2022-10-26 18:01:14 +0700

    #5 get_request_url impl + file.json

--- google.py
+++ google.py
@@ -1,3 +1,13 @@
 from bs4 import BeautifulSoup as bs
+import json
 
-bs.get()
+
+def get_requests_url(path):
+    with open(path) as json_file:
+        data = json.load(json_file)
+
+        print(data.values())
+
+
+# read_json("file.json")
+get_requests_url("file.json", "123123")
