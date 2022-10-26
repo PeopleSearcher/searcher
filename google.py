0711ea8210fb99ad5f8b63f856dbbefc4c948fce
Sewa <hoteroke@gmail.com>
2022-10-26 23:54:49 +0700

    #5 * Modified file.json * Modified google.py * useragents.txt added

--- google.py
+++ google.py
@@ -1,13 +1,33 @@
+import random
+
 from bs4 import BeautifulSoup as bs
+import requests
 import json
 
+json_file = open("file.json")
+data = json.load(json_file)
+useragents = open('useragents.txt').read().splitlines()
 
-def get_requests_url(path):
-    with open(path) as json_file:
-        data = json.load(json_file)
 
-        print(data.values())
+def make_request(search_type: str, search_data:str):
+    search_descriptions = data[search_type].values()
+    for description in search_descriptions:
+        user_agent = random.choice(useragents)
+        headers = {
+            "User-Agent": user_agent
+        }
+        formatted_url = description["url"].format(field=search_data)
+        # TODO solve yandex problem(captcha)
+        response = requests.get(formatted_url, headers=headers)
+        if response.status_code == 200:
+            soup = bs(response.text, "html.parser")
+            cls = {}
+            if 'class' in description:
+                cls = {"class": description["class"]}
+            answers = soup.findAll(description["search"], cls)
+            print(answers)
+            print("="*20)
 
 
 # read_json("file.json")
-get_requests_url("file.json", "123123")
+make_request("fio", "Власов Александр Александрович")
