2530b5504a628f7d71afafa8e274c16e45d5e23a
Sewa <hoteroke@gmail.com>
2022-11-07 14:16:47 +0700

    * data.py change parsing, now it can parse changed operator * models_tests.py changes due to data.py.Phone return another json data

--- models/data.py
+++ models/data.py
@@ -1,8 +1,11 @@
 import random
 
-from pydantic import BaseModel, validator, root_validator
+from pydantic import BaseModel, validator, root_validator, ValidationError
 import requests
 from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 useragents = open('../useragents.txt').read().splitlines()
 
 
@@ -14,23 +17,38 @@ class Phone(BaseModel):
     country: str | None = None
     region: str | None = None
     operator: str | None = None
+    additional: str | None = None
 
     @root_validator
     def parse_phone(cls, values):
-        values["phone_num"] = values["phone_num"].strip().replace(' ', '').replace('-', '').replace('(', '').replace(')', '')
-        values["phone_num"] = "+" + values["phone_num"] if values["phone_num"][0] != "+" and len(values["phone_num"]) >= 11 else values["phone_num"]
-        if len(values["phone_num"]) == 12:
-            response = requests.get(f"https://fincalculator.ru/telefon/region-po-nomeru/{values['phone_num']}")
-            soup = bs(response.text, features='html.parser')
-            divs = soup.findAll('div', {'class': "tel-info_result-label"})
-            for div in divs:
-                if div.text == 'Страна : ':
-                    values["country"] = div.nextSibling.text.strip()
-                if div.text == 'Регион : ':
-                    values['region'] = div.nextSibling.text.strip()
-                if div.text == 'Оператор : ':
-                    values['operator'] = div.nextSibling.text.strip()
-        return values
+        if values["phone_num"] is not None:
+            values["phone_num"] = values["phone_num"].strip().replace(' ', '').replace('-', '').replace('(',
+                                                                                                        '').replace(')',
+                                                                                                                    '')
+            values["phone_num"] = "+" + values["phone_num"] if values["phone_num"][0] != "+" and len(
+                values["phone_num"]) >= 11 else values["phone_num"]
+            if len(values["phone_num"]) == 12:
+                options = webdriver.ChromeOptions()
+                options.add_argument("--headless")
+                driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+                driver.get(f"https://phonenum.info/phone/{values['phone_num'][1:]}")
+                html = driver.page_source
+                driver.quit()
+                soup = bs(html, features='html.parser')
+                not_ported = soup.find("span", {"class": "phone_not_ported_message"})
+                if not_ported["style"].strip() == "display:none":
+                    values['additional'] = not_ported.nextSibling.nextSibling.text.strip().replace("\n", " ")
+                data = soup.find("div", {"class": 'diapason'})
+                if values["phone_num"][1:-10] == "7":
+                    values["country"] = "Россия"
+                values[
+                    'region'] = data.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.text
+                values[
+                    'operator'] = data.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling. \
+                    previousSibling.previousSibling.previousSibling.previousSibling.text
+            return values
+        else:
+            raise ValueError("phon_num is not presented")
 
     @property
     def country_code(self):
@@ -45,9 +63,11 @@ class Phone(BaseModel):
         return self.phone_num[-7:] if len(self.phone_num) >= 12 else None
 
     def __str__(self):
+        res = f"Телефон: {self.phone_num}"
         if self.country_code is not None:
-            return f"Телефон: {self.phone_num}\nСтрана: {self.country}({self.country_code})\n" \
+            res += f"\nСтрана: {self.country}({self.country_code})\n" \
                    f"Принадлежность к региону: {self.region}({self.operator_code})\n" \
                    f"Оператор: {self.operator}\nКороткий номер: {self.number}"
-        else:
-            return f"Телефон: {self.phone_num}"
+            if self.additional is not None:
+                res += f"\n{self.additional}"
+        return res
--- tests/models_tests.py
+++ tests/models_tests.py
@@ -7,15 +7,18 @@ from models.data import Phone
 class PhoneTests(unittest.TestCase):
     def test_full(self):
         phone_num = 79142900258
-        expected = {"phone_num": "+79142900258", "country": "Россия", "region": "Республика Саха-Якутия", "operator": "MTC"}
+        expected = {'additional': None, 'country': 'Россия', 'operator': 'ПАО "Мобильные ТелеСистемы"', 'phone_num': '+79142900258', 'region': 'Республика Саха /Якутия/'}
         phone = Phone(phone_num=phone_num)
 
         self.assertEqual(expected, json.loads(phone.json()))
 
     def test_formatting(self):
         phone_num = '+7 (914) 290-02-58'
-        expected = {"phone_num": "+79142900258", "country": "Россия", "region": "Республика Саха-Якутия",
-                    "operator": "MTC"}
+        expected = {'additional': None,
+                    'country': 'Россия',
+                    'operator': 'ПАО "Мобильные ТелеСистемы"',
+                    'phone_num': '+79142900258',
+                    'region': 'Республика Саха /Якутия/'}
         phone = Phone(phone_num=phone_num)
         self.assertEqual(expected, json.loads(phone.json()))
 
